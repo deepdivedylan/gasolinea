1c9743e0b91196a755610b98327510e15406c34d
Dylan McDonald <dmcdonald21@cnm.edu>
2019-11-02 21:45:25 -0700

    added failsafe that prevents duplicate data

--- mysql/scripts/price-insert.sql
+++ mysql/scripts/price-insert.sql
@@ -1,13 +1,17 @@
 DROP PROCEDURE IF EXISTS addPrice;
 
 DELIMITER $$
-CREATE PROCEDURE addPrice(IN gasTypeId TINYINT UNSIGNED, IN gasPlaceId MEDIUMINT UNSIGNED, gasPrice FLOAT)
+CREATE PROCEDURE addPrice(IN typeId TINYINT UNSIGNED, IN gasPlaceId MEDIUMINT UNSIGNED, gasPrice FLOAT)
 	BEGIN
+	   DECLARE gasPriceExists TINYINT UNSIGNED;
 		DECLARE gasStationExists TINYINT UNSIGNED;
 
 		SELECT COUNT(placeId) INTO gasStationExists FROM mexGasStation WHERE placeId = gasPlaceId;
 		IF gasStationExists = 1 THEN
-			INSERT INTO mexGasPrice(gasTypeId, placeId, priceDate, price) VALUES(gasTypeId, gasPlaceId, CURRENT_DATE(), gasPrice);
+		   SELECT COUNT(gasTypeId) INTO gasPriceExists FROM mexGasPrice WHERE gasTypeId = typeId AND placeId = gasPlaceId AND priceDate = CURRENT_DATE();
+		   IF gasPriceExists = 0  THEN
+				INSERT INTO mexGasPrice(gasTypeId, placeId, priceDate, price) VALUES(typeId, gasPlaceId, CURRENT_DATE(), gasPrice);
+			END IF;
 		END IF;
 
 	END $$

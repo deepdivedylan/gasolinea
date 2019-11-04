3d81db2e4697427e7e1e91cab2de40ed7ffc9b95
Dylan McDonald <dmcdonald21@cnm.edu>
2019-11-03 16:36:28 -0800

    improved stored procedure to get current prices

--- mysql/scripts/get-current-prices.sql
+++ mysql/scripts/get-current-prices.sql
@@ -1,25 +1,9 @@
 DROP PROCEDURE IF EXISTS getCurrentPrices;
 
 DELIMITER $$
-CREATE PROCEDURE getCurrentPrices()
+CREATE PROCEDURE getCurrentPrices(OUT currExchangeRate FLOAT, OUT latestDate DATE)
 	BEGIN
-	   DECLARE currPrice FLOAT;
-	   DECLARE currCity VARCHAR(32);
-	   DECLARE currGasType VARCHAR(8);
-		DECLARE latestDate DATE;
-	   DECLARE done BOOLEAN DEFAULT FALSE;
-	   DECLARE gasCursor CURSOR FOR SELECT AVG(price) AS price, municipio.name AS municipio, gasType.name AS gasType
-											FROM mexGasPrice
-														INNER JOIN mexGasStation ON mexGasPrice.placeId = mexGasStation.placeId
-														INNER JOIN gasType ON mexGasPrice.gasTypeId = gasType.gasTypeId
-														INNER JOIN municipio ON mexGasStation.municipioId = municipio.municipioId
-											WHERE priceDate = latestDate
-											GROUP BY municipio, gasType
-											UNION SELECT price, 'San Diego' AS municipio, gasType.name AS gasType
-											FROM usaGasPrice
-														INNER JOIN gasType ON usaGasPrice.gasTypeId = gasType.gasTypeId
-											WHERE priceDate = latestDate;
-		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+	   DECLARE galToLiter FLOAT DEFAULT 3.78541;
 
 		SELECT DISTINCT priceDate INTO latestDate
 		FROM usaGasPrice
@@ -27,21 +11,22 @@ CREATE PROCEDURE getCurrentPrices()
 		ORDER BY priceDate DESC
 		LIMIT 1;
 
-		DROP TEMPORARY TABLE IF EXISTS priceSummary;
-		CREATE TEMPORARY TABLE priceSummary (
-			price FLOAT NOT NULL,
-			city VARCHAR(32) NOT NULL,
-			gasType VARCHAR(8) NOT NULL
-		);
+		SELECT exchangeRate INTO currExchangeRate
+		FROM exchangeRate
+	   ORDER BY exchangeDate DESC
+	   LIMIT 1;
 
-		OPEN gasCursor;
-
-		mexLoop: LOOP
-			FETCH gasCursor INTO currPrice, currCity, currGasType;
-			INSERT INTO priceSummary(price, city, gasType) VALUES(currPrice, currCity, currGasType);
-		END LOOP;
-
-		SELECT price, city, gasType FROM priceSummary;
+		SELECT AVG(price) AS price, municipio.name AS municipio, gasType.name AS gasType
+		FROM mexGasPrice
+			INNER JOIN mexGasStation ON mexGasPrice.placeId = mexGasStation.placeId
+			INNER JOIN gasType ON mexGasPrice.gasTypeId = gasType.gasTypeId
+			INNER JOIN municipio ON mexGasStation.municipioId = municipio.municipioId
+		WHERE priceDate = latestDate
+		GROUP BY municipio, gasType
+		UNION SELECT ((currExchangeRate * price) / galToLiter) AS price, 'San Diego' AS municipio, gasType.name AS gasType
+		FROM usaGasPrice
+			INNER JOIN gasType ON usaGasPrice.gasTypeId = gasType.gasTypeId
+		WHERE priceDate = latestDate;
 
 	END $$
 

0c374b3a23f25762f69e7bc5818c8b73cc939143
Dylan McDonald <dmcdonald21@cnm.edu>
2019-11-03 18:38:10 -0800

    added npm config

---
+++ site/public/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once(dirname(__DIR__) . "/php/intl/Translator.php");
+
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+if (empty($_SESSION["locale"]) === true) {
+	$locale = Translator::getLocale();
+	$_SESSION["locale"] = $locale;
+	setcookie("locale", $locale, time() + 2592000); // 30 day cookie
+} else {
+	$locale = $_SESSION["locale"];
+}
+$translator = new Translator($locale);
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $locale; ?>">
+	<head>
+		<title><?php echo $translator->getTranslatedString("title"); ?></title>
+	</head>
+	<body>
+		<h1><?php echo $translator->getTranslatedString("title"); ?></h1>
+	</body>
+</html>
